bb5bf70c6dd92801e8d35044f764cc1aac3543b5
Igor Pereira <pereira.t.igor@gmail.com>
2023-11-22 15:25:40 -0300

    Implemented Context API so buttonhandles theme switching

--- src/App.js
+++ src/App.js
@@ -10,10 +10,8 @@ import Layout from './components/Layout';
 function App() {
   return (
     <ThemeSwitcher>
-      {/* <ThemeProvider theme={themes[currentTheme]}> */}
         <GlobalStyle />
         <Layout />
-      {/* </ThemeProvider> */}
     </ThemeSwitcher>
   );
 };
